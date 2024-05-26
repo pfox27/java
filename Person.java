e7a743dbb585fb863bda3ab5ddb4fb820ba2fa25
pfox27 <pfox27@gmail.com>
2024-05-26 17:26:52 -0400

    update

---
+++ Person.java
@@ -0,0 +1,13 @@
+/* This is a class to be used with the runnerPerson.java program.  It defines the attributes of the object "Person". */
+
+private class Person{
+	private String nameLast;
+	private String nameFirst;
+	private int age;
+	private String ssn;
+	
+public Person(String F, String L, int A, String S){
+	 nameLast = L;
+	 nameFirst = F;
+	 age = A;
+	 ssn = S;
