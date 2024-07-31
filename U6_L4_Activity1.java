b80e05677ef6a8e56302b61d11266f35a039ad10
Paul Fox <pfox27@gmail.com>
2024-07-31 10:41:05 -0400

    update

--- U6_L4_Activity1.java
+++ U6_L4_Activity1.java
@@ -1,6 +1,14 @@
 public class U6_L4_Activity1
 {
-public static void insert(String[] words String newWord int place) {
+public static void insert(String() words[] String newWord int place) {
   // Write your insert method here
 
+		for (int i = words.length - 1; i > place - 1; i--)
+			{
+  				words[i] = words[i - 1];
+			}
+		words[place - 1] = newWord;
+	
+		return newWord;
+	}
 }
