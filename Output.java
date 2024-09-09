52bd34bacf3cafd38eea6c74bcb241fd0ece2c01
Paul Fox <pfox27@gmail.com>
2024-09-09 15:43:05 -0400

    update

---
+++ Output.java
@@ -0,0 +1,18 @@
+/* This program will output a Java "banner" to the screen, using asterisks. */
+
+public class Output{
+
+	public static void main (String[] args){
+
+		System.out.println("********");
+		System.out.println("* Java *");
+        System.out.println("********");
+	}
+}
+
+
+
+
+
+
+
