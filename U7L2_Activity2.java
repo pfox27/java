581e158ab1dc29f589509ae487495aa14392ae99
pfox27 <80210760+pfox27@users.noreply.github.com>
2025-02-21 08:16:18 -0500

    update

--- U7L2_Activity2.java
+++ U7L2_Activity2.java
@@ -1,3 +1,5 @@
+/* U7L2_Activity2.java - A method to find the highest integer in an ArrayList. */
+
 import java.util.ArrayList;
 
 public class U7L2_Activity2
@@ -5,16 +7,17 @@ public class U7L2_Activity2
 
   // write your highestNum method here
 
-	int maxNum = 0;
+	static int maxNum = 0;
 
-	public static int highestNum(int values){
-		 ArrayList<int> numList = new ArrayList<int>();
+	public static int highestNum(ArrayList<Integer> values){
+		 //ArrayList<Integer> numList = new ArrayList<Integer>();
                  for(int i = 0; i < values.size(); i++){
 		 	int currentNum = values.get(i);
 			if(currentNum > maxNum){
 				maxNum = currentNum;
 			}
-		return maxNum;	
+			
 		}
+	   return maxNum;
 	}
 }
