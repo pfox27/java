1595ea02dc31fd0dec365edc1ff0967b32235cd3
pfox27 <pfox27@gmail.com>
2025-03-29 09:53:05 -0400

    update

---
+++ BinomialExpansion.java
@@ -0,0 +1,31 @@
+/*  A program that calculates the perimeter of a regular polygon given the number of sides and the side length.       */
+
+import java.util.Scanner;
+
+public class BinomialExpansion{
+
+public static void timeOfDay(int x) {
+	System.out.println("Binomial Expansion Coefficient Generator");
+	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+
+
+
+public static void main(String Args[]){
+
+Scanner sc = new Scanner(System.in);
+System.out.println("\nPlease input the current hour of the day (24-hour clock):");
+int hourOfDay = sc.nextInt();
+timeOfDay(hourOfDay);
+
+
+	}
+}
+
+
+
+
+
+
+
+
