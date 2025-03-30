968e6084c4be1ca2911ea538f53a1fa9aa312907
pfox27 <pfox27@gmail.com>
2025-03-29 22:00:34 -0400

    update

--- BinomialExpansion.java
+++ BinomialExpansion.java
@@ -1,31 +1,46 @@
-/*  A program that calculates the perimeter of a regular polygon given the number of sides and the side length.       */
+/*  A program that computes and then outputs the coefficients for a binomial expansion.*/
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class BinomialExpansion{
 
-public static void timeOfDay(int x) {
+public static int main(String[] args) {
+
+	int[] coeff;
+	int nFact = 0;
+	int kFact = 0;
+	int nkFact = 0;
+
 	System.out.println("Binomial Expansion Coefficient Generator");
 	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	System.out.println("\nPlease enter the exponent for the binomial as a positive integer:");
+	Scanner scan = new Scanner(System.in);
+	int n = scan.nextInt(); // value of exponent stored as n
+	nFact = n;
 
+	// Now we compute n!
+	for(int i = n; i >= 2; i--){
+		nFact = nFact*(i - 1);
+	}
+	
+	for(int k = 1; k <= n; k++){
+		//Compute k!
+		kFact = k;
+		
+		for(int j = k; j >= 2; j--){
+		kFact = kFact*(j - 1);
+		}	
+			nkFact = n - k;
+			for(int p = n - k; p >= 2; p--){
+				nkFact = nkFact*(i - 1);
+			}	
 
-
-
-public static void main(String Args[]){
-
-Scanner sc = new Scanner(System.in);
-System.out.println("\nPlease input the current hour of the day (24-hour clock):");
-int hourOfDay = sc.nextInt();
-timeOfDay(hourOfDay);
-
+		coeff[k] = nFact/(kFact*nkFact);
 
 	}
+	
+	System.out.println("The Coefficients of the binomial expansion are: " + Arrays.toString(coeff));
+	}
 }
-
-
-
-
-
-
-
 
