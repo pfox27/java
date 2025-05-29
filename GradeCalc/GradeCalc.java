66f5569a2fd6e0f917f930ea86b2d859aa36fd1e
pfox27 <80210760+pfox27@users.noreply.github.com>
2025-05-29 16:02:15 -0400

    update

--- GradeCalc/GradeCalc.java
+++ GradeCalc/GradeCalc.java
@@ -40,6 +40,25 @@ public class GradeCalc {
 		double catWeightingA = 0.0;
 		double catWeightingC = 0.0;
 		double catWeightingT = 0.0;
+		double knowledgeScoreSum = 0;
+		double applicationScoreSum = 0;
+		double thinkingScoreSum = 0;
+		double communicationScoreSum = 0;	
+		double knowledgeOutOfSum = 0;
+		double applicationOutOfSum = 0;
+		double thinkingOutOfSum = 0;
+		double communicationOutOfSum = 0;
+		double[] termMark = new double[nCourses];
+		String[] aName = new String[nAssess];
+		double[] assessWeighting = new double[nAssess];
+		double[] assessOutOfK = new double[nAssess];
+		double[] assessOutOfA = new double[nAssess];
+		double[] assessOutOfT = new double[nAssess];
+		double[] assessOutOfC = new double[nAssess];
+		double[] assessScoreK = new double[nAssess];
+		double[] assessScoreA = new double[nAssess];
+		double[] assessScoreT = new double[nAssess];
+		double[] assessScoreC = new double[nAssess];
 		
 		for(int i = 0; i < numCourses; i++){
 			
@@ -55,14 +74,49 @@ public class GradeCalc {
 			catWeightingT = scan.nextDouble();
 			scan.nextLine();
 			System.out.println("Please enter the number of assessments in " + cName + ":");
-			catWeightingK = scan.nextDouble();
+			int numAssess = scan.nextInt();
 			
 			courses[i] = new Course(cName, catWeightingK, catWeightingA, catWeightingC, catWeightingT);
-		
+
+				for(t = 0; u < numAssess; u++){
+					System.out.println("Please enter the name of assessment " + u + ":");
+					aName[u] = scan.nextLine();					
+					System.out.println("Please enter the weight (10/30/50) for " + aName[u] + ":");
+					aWeighting[u] = scan.nextDouble();
+					System.out.println("Please enter what knowledge for " + aName[u] + "is out of:");
+					assessOutOfK[u] = scan.nextDouble();
+					System.out.println("Please enter the knowledge score for " + aName[u] + ":");
+					assessScoreK[u] = scan.nextDouble();
+					System.out.println("Please enter what application for " + aName[u] + "is out of:");
+					assessOutOfA[u] = scan.nextDouble();
+					System.out.println("Please enter the application score for " + aName[u] + ":");
+					assessScoreA[u] = scan.nextDouble();
+					System.out.println("Please enter what thinking for " + aName[u] + "is out of:");
+					assessOutOfT[u] = scan.nextDouble();
+					System.out.println("Please enter the thinking score for " + aName[u] + ":");
+					assessScoreT[u] = scan.nextDouble();
+					System.out.println("Please enter what communication for " + aName[u] + "is out of:");
+					assessOutOfC[u] = scan.nextDouble();
+					System.out.println("Please enter the communication score for " + aName[u] + ":");
+					assessScoreC[u] = scan.nextDouble();
+					knowledgeScoreSum = knowledgeScoreSum + assessScoreK[u]/assessOutOfK[u]*aWeighting[u];
+					applicationScoreSum = applicationScoreSum + assessScoreA[u]/assessOutOfA[u]*aWeighting[u];
+					thinkingScoreSum = thinkingScoreSum + assessScoreT[u]/assessOutOfT[u]*aWeighting[u];
+					communicationScoreSum = communicationScoreSum + assessScoreC[u]/assessOutOfC[u]*aWeighting[u];
+					knowledgeOutOfSum = knowledgeOutOfSum + aWeighting[u];
+					applicationOutOfSum = applicationOutOfSum + aWeighting[u];
+					thinkingOutOfSum = thinkingOutOfSum + aWeighting[u];
+					communicationOutOfSum = communicationOutOfSum + aWeighting[u];
+					termMark[i] = knowledgeScoreSum/knowledgeOutOfSum*c[i].weightK + applicationScoreSum/applicationOutOfSum*c[i].weightA + thinkingScoreSum/thinkingOutOfSum*c[i].weightC + communicationScoreSum/communicationOutOfSum*c[i].weightT;
+					System.out.println("Term mark for " + c[i].name + " is:" + termMark[i]);					
+					Asess[] assessments = new Assess[numAssess];
+					assessments[t] = new Assess(aName[u], aWeighting[u], assessOutOfK[u], assessOutOfA[u], assessOutOfC[u], assessOutOfT[u], assessScoreK[u], assessScoreA[u], assessScoreC[u], assessScoreT[u]
+
+
 		}
 //	Assessments.assessments(numCourses, courses);
-
-		
+			
+			
 	}
 
 }
